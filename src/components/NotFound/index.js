d0d991fbf1c397b5ad12a315f7f09d34a89c19d3
kumaraswamy <kumarkumaraswamy77@gmail.com>
2026-01-06 13:57:05 +0000

    Solution

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,10 @@
 import './index.css'
 
 const NotFound = () => (
-  <div className='not-found-container'>
+  <div className="not-found-container">
     <img
-      src='https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png'
-      alt='not found'
+      src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+      alt="not found"
     />
     <h1>Page Not Found</h1>
     <p>We are sorry, the page you requested could not be found</p>
